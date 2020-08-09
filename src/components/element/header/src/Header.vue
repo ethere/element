4f0d4f69ac009703107fb274eec1775e035be44f
ethere <3318216036@qq.com>
2020-08-09 09:31:36 +0800

    write other container

--- src/components/element/header/src/Header.vue
+++ src/components/element/header/src/Header.vue
@@ -1,10 +1,18 @@
 <template>
-    <div class="el-header">
+    <div
+    class="el-header"
+    :style="{'height':height}">
         <slot></slot>
     </div>
 </template>
 <script>
 export default {
   name: 'ElHeader',
+  props: {
+    height: {
+      type: String,
+      default: '60px',
+    },
+  },
 };
 </script>
